55453d45af67d61faa5683a8d34af5988a0421e0
yan-gabala <vinokur.v.v@yandex.ru>
2023-08-03 10:00:07 +0000

    Smallest fix

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', 'false').lower()=='true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['51.250.29.210', '127.0.0.1', 'localhost', 'devinse.ru']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
